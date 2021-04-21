59963a24f8f00c689410b1b083247aebfcb0e164
filipovid <Whereismyrepo1>
2021-04-21 15:25:13 +0200

    Adding array of products

--- src/pm/productapp/app/Shop.java
+++ src/pm/productapp/app/Shop.java
@@ -10,15 +10,22 @@ public class Shop {
     public static void main(String[] args) {
         // Instantiating class ProductManager - invoking createProduct methods for creating a new Product
         ProductManager pm = new ProductManager(Locale.UK);
+
         Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
         pm.printProductReport();
 
         p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Nice hot cup of tea");
+        p1 = pm.reviewProduct(p1, Rating.TWO_STAR, "Rather weak tea");
+        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Fine tea");
+        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Good tea");
+        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Perfect tea");
+        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Just add some lemon");
+
         pm.printProductReport();
 
         /**
          * Next two objects are used for purpose of comparing objects with equals() method
-         * equals() method is overridden in pm.productapp.data.Product class
+         * equals() method is overridden in Product class
          */
 //        Product p6 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
 //        Product p7 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
